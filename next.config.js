3ef3e0d666603c95fbe9392e7f1e6dfe17d6b85d
KleoToubi <toumpanos5@gmail.com>
2025-06-09 00:16:41 +0200

    fix: resolve merge conflict in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,11 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
+  experimental: {
+    serverActions: true,
   },
   images: {
-    unoptimized: true,
+    domains: ['res.cloudinary.com'],
   },
-  trailingSlash: false,
-  output: "standalone",
 }
 
 module.exports = nextConfig
-=======
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development"
-});
-
-module.exports = withPWA({
-  reactStrictMode: true,
-});
->>>>>>> c9fe30550c3a62256f8582dada57c1b844eb0af0
